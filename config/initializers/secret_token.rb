985428c3a2fdbab866f1f4d65fe9c128f8025bfa
Stefan Botzenhart <sb@basiszwo.com>
2010-12-28 16:57:42 +0100

    initial commit;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pfdemo::Application.config.secret_token = 'bbb23c4b3ff0cef41513f80e930963ce1a55dc6a9a3d2dfc2f7579a076bfd7612075cd6f038e4ca7ef02a7b2697600c1f3fd7f83d39f0c7de4529dd6e18a2aa9'
