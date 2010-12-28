b965bee1494bed1cf6224c716e8368c4f9dbb95a
Stefan Botzenhart <sb@basiszwo.com>
2010-12-28 19:57:06 +0100

    simplify redirect after sign_in for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   
   private
 
-    def after_sign_in_path_for(resource_or_scope)
-      edit_user_registration_path
-    end
+    # def after_sign_in_path_for(resource_or_scope)
+    #   edit_user_registration_path
+    # end
 
     # Overwriting the sign_out redirect path method
     # def after_sign_out_path_for(resource_or_scope)
