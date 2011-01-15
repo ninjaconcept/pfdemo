0341abda025ac05974b831c4113d95170ca5e80c
Stefan Botzenhart <sb@basiszwo.com>
2011-01-15 13:43:31 +0100

    use rvm env for deployment env staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -38,3 +38,10 @@ set :repository, "git@github.com:ninjaconcept/#{application}.git"
 set :deploy_via, :export
 #set :deploy_via, :remote_cache
 #set :git_enable_submodules, 1
+
+
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2@pfdemo'      # Or whatever env you want it to run in.
+set :rvm_type, :user
